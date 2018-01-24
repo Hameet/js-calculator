d122e10ff3176c1496e137b7ae9123411af51ff2
hameet-singh <32632890+Hameet@users.noreply.github.com>
2018-01-24 22:37:06 +1300

    Delete calculator.js

--- calculator.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener ("DOMContentLoaded", function () {
-
-  var buttons = document.getElementById('buttons');
-
-  for (var i = 0; i <buttons.length; i++) {
-      buttons[i].addEventListener('click', buttonClicked);
-  }
-});
